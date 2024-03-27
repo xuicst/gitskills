5a2c287142a1aa946f230d3b13d6322d4841a669
xuicst <122449450+xuicst@users.noreply.github.com>
2024-03-27 12:58:19 +0000

    modified:   README.md 	modified:   test.py

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 > 联动本地vscode与github codespace
 > *PS*：使用起来感觉还是不错的
 
+## 一、Tools
 工具：jupytext
 非常好用
-```shell
+```
 # 使用方法：
 jupytext --to notebook your_file.md 
+```
+
+## 二、PYthon TesT
+> 利用python代码测试所学
+
+
+## 三、LaTex
---
+++ test.py
@@ -0,0 +1,23 @@
+import datetime
+
+class Golem:
+
+    def __init__(self, name=None):   #__init__为class中的一个method
+        self.name = name
+        self.built_year = datetime.date.today().year
+            
+    def say_i(self):
+        print("Say Hi!")
+
+class Running_Golem(Golem):
+
+    def run(self):
+        print("Can't you see ? I'm here waiting for you...")
+
+rg = Running_Golem('Galiy')
+
+rg.run()
+rg.run
+rg.built_year
+rg.name
+rg.say_i()
